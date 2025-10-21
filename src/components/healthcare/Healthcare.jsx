f82964ec962e0c169b3a1548206aaeca7f9c1b8a
Mohirjon <mohirjon@example.com>
2025-10-21 15:51:46 +0500

    added helathcore page

--- src/components/healthcare/Healthcare.jsx
+++ src/components/healthcare/Healthcare.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Healthcare = () => {
   return (
-    <div>
-      Healthcare
+    <div className='healthcare'>
+     <div className="text">
+      <h1>Virtual healthcare <br /> for you</h1>   
+
+      <p>Trafalgar provides progressive, and affordable  <br />
+         healthcare, accessible on mobile and online <br />
+         for everyone 
+      </p>  
+      </div>
     </div>
   )
 }
