d442d0e86636688156067fa364b7c4d02576a48d
rustamov 902407757 <abdirahmonrustamov7757@gmail.com>
2025-10-21 15:41:29 +0500

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div>
-      Footer
+    <footer>
+           <div className="header__logo flex items-center gap-[12px]">
+              <div className="header__logo--circle w-[41px] h-[41px] bg-[#458FF6] rounded-[50%] flex items-center justify-center mullish text-[26px] text-amber-50 font-bold">
+                <span>T</span>
+              </div>
+              <Link to={"/"} className='mullish font-medium text-[24px] text-[#233348]'>Trafalgar</Link>
+            </div>
+
+    </footer>
     </div>
   )
 }
